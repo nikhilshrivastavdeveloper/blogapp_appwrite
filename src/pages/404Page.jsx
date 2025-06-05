e1b131ef22dc83fa818e93ac1cfb28b47f6f4ec4
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-06-06 01:16:21 +0530

    add new page 404 page

---
+++ src/pages/404Page.jsx
@@ -0,0 +1,10 @@
+
+function NotFound(){
+  return (
+    <div className="flex justify-center items-center h-full overflow-hidden my-[30px]">
+    <img src="/images/404_error_page_not_found.gif" alt="not_found" className="mr-[45px] sm:mr-[0px]"/>
+    </div>
+  )
+}
+
+export default NotFound

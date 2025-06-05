34135572571188c39c807b27c335a318abb737c4
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-06-06 01:18:57 +0530

    improve css and make more responsive

--- src/components/PostCard/Postcard.jsx
+++ src/components/PostCard/Postcard.jsx
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 function Postcard({ $id, title, featuredImage }) {
     return (
         <Link to={`/post/${$id}`}>
-            <div className='w-full bg-gray-100 rounded-xl p-4'>
-                <div className='w-full justify-center mb-4'>
+            <div className='w-full h-full bg-gray-100 rounded-[5px] border-[1px] border-black/50'>
+                <div className='w-1/1 h-1/2 justify-center mb-4 bg-white rounded-tl-[5px] rounded-tr-[5px]'>
                     <img src={databaseService.getFilePreview(featuredImage)} alt={title}
-                        className='rounded-xl' />
+                        className='rounded-tl-[5px] rounded-tr-[5px] object-cover w-full h-full border-black/50' />
                 </div>
-                <h2
-                    className='text-xl font-bold'
-                >{title}</h2>
+                <div className="p-[5px]">
+                    <h2
+                        className='text-xl font-bold'
+                    >{title}</h2>
+                </div>
             </div>
         </Link>
     )
 }
 
-export default Postcard;
+export default Postcard;    

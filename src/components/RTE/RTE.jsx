14eb56cb57a23811b32acc41288dd8d1c8597c19
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-06-09 20:50:42 +0530

    remove some permission

--- src/components/RTE/RTE.jsx
+++ src/components/RTE/RTE.jsx
@@ -24,7 +24,6 @@ function RTE({ name = "content", label, control, defaultValue = "" }) {
                                 "autolink",
                                 "lists",
                                 "link",
-                                "image",
                                 "charmap",
                                 "preview",
                                 "anchor",
@@ -41,7 +40,7 @@ function RTE({ name = "content", label, control, defaultValue = "" }) {
                                 "anchor",
                             ],
                             toolbar:
-                                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                                "undo redo | blocks | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
                             content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
                         }}
                         onEditorChange={onChange}

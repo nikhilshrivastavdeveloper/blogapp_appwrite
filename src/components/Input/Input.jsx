d6873856972cac083d157093ed3a7aaec8831d54
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-06-14 18:45:42 +0530

    add a new input field radio input field

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,8 @@
 import { useId, forwardRef } from "react"
 
-const InputBox = forwardRef(({label, type="text", className="", ...props }, ref) => {
+const InputBox = forwardRef(({ label, type = "text", className = "", ...props }, ref) => {
     const id = useId()
-    
+
     return (
         <div className="w-full">
             {
@@ -13,4 +13,24 @@ const InputBox = forwardRef(({label, type="text", className="", ...props }, ref)
     )
 })
 
+const RadioBox = forwardRef(({ label, className = "", options = [], name, ...props }, ref) => {
+    return (
+        <div className="w-full flex items-center">
+            {
+                label && <label className='inline-block mb-1 pl-1'>{label}</label>
+            }
+            {
+                options.length > 0 && options.map((option,index) => (
+                    <div key={option}>
+                        <input type="radio" name={name} ref={ref} value={option} id={index}  className={`inline-block mx-[10px] mt-[10px] ${className}`} 
+                        {...props} />
+                        <label htmlFor={index} className='inline-block mb-1 pl-1'>{option}</label>
+                    </div>
+                ))
+            }
+        </div>
+    )
+})
+
 export default InputBox
+export {RadioBox}

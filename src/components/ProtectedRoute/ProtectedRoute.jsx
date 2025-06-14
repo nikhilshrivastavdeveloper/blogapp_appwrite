c1cd120ab4ed9810eb814a58fedd25df2691f3d7
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-06-14 18:52:30 +0530

    remove comments

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,27 +1,6 @@
 import { useSelector } from "react-redux"
 import { Navigate } from "react-router-dom";
 
-// function ProtectedRoute({ children, authentication = true }) {
-//     const authStatus = useSelector((state) => state.auth.status)
-//     const navigate = useNavigate()
-//     const [loader, setLoader] = useState(true)
-
-//     useEffect(() => {
-//         // true && true
-//         if (authentication && !authStatus){
-//             //if user have account but not login so redirect it on login page
-//             navigate("/login")
-//         } else if (!authentication && !authStatus) {
-//             //if user have no account and no login so redirect it on signup page
-//             navigate("/signup")
-//         }
-
-//         setLoader(false)
-//     }, [authStatus, authentication, navigate])
-
-//     return loader ? <div>Loading...</div> : children
-// }
-
 function ProtectedRoute({children}){
     const authStatus = useSelector((state) => state.auth.status);
 

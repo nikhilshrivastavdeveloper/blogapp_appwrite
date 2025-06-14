d77c5f315e68211a4501f2b4b9c076c8fc0ffd1d
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-06-14 18:50:23 +0530

    add hard code option tag

--- src/components/SelectDropdown/Select.jsx
+++ src/components/SelectDropdown/Select.jsx
@@ -9,6 +9,7 @@ function Select({ options = [], label, className = "", reference, ...props },ref
                         label && <label htmlFor={id}>{label}</label>
                     }
                     <select id={id} {...props} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
+                        <option value="">select</option>
                         {
                             options.length > 0 && options.map((option) => (
                                     <option key={option} value={option}>{option}</option>

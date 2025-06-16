df6659a8ac3f7d1ca55cf5bf59480685ac30f183
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-06-16 23:11:40 +0530

    add new logic like fallback ui if post not found

--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,26 +1,32 @@
 import databaseService from "../appwrite/database.js";
 import { useState, useEffect } from "react";
-import { Container, Postcard, NoContent } from "../components/index.js";
+import { Container, Postcard, NoContent, Loader } from "../components/index.js";
 
 function AllPost() {
     const [allPost, setAllPost] = useState([]);
+
+    const [loader, setLoader] = useState(true)
 
     useEffect(() => {
         databaseService.getPosts().then((posts) => {
             if (posts) {
                 setAllPost(posts.documents)
             }
+        }).finally(() => {
+            setLoader(false)
         })
     }, [])
+
+    if (loader) return <Loader />
 
     return (
         <div className='w-full py-5'>
             <Container>
-                <div className='flex flex-wrap sm:gap-[10px]'>
+                <div className='flex flex-wrap'>
                     {
                         allPost.length > 0 ? (
                             allPost.map((post) => (
-                                <div key={post.$id} className='w-1/2 sm:w-1/4 p-[2px] sm:p-[0px] '>
+                                <div key={post.$id} className='w-1/1 sm:w-1/2 md:w-1/3 lg:w-1/4 py-[5px] sm:p-[5px] sm:py-[10px] min-h-full'>
                                     <Postcard  {...post} />
                                 </div>
                             ))
